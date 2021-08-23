cd2ae8afd1d3267b3861503c8e3b33cc3251a2fc
Kirill_Morozov <kirill_morozov@epam.com>
2021-08-23 19:23:26 +0300

    schema enabled

--- samples/plugins/postgres_jdbc/connectionBuilder.js
+++ samples/plugins/postgres_jdbc/connectionBuilder.js
@@ -1,6 +1,6 @@
 (function dsbuilder(attr) {
-//    var urlBuilder = "jdbc:trino://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase] + "?";
-    var urlBuilder = "jdbc:trino://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "?";
+    var urlBuilder = "jdbc:trino://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase] + "?";
+//    var urlBuilder = "jdbc:trino://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "?";
 
     return [urlBuilder];
 })
